22172fdb5883448ddb5f23c2bb74cb64179d6c08
Sergey <physiho@yahoo.com>
2023-07-04 20:57:36 +0400

    Add Swagger documentation

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 from .views import create_task, get_all_tasks, task
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Tasks_Django",
+      default_version='v1',
+      description="API documentation for Tasks API",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('tasks/', get_all_tasks, name='get_all_tasks'),
     path('tasks/create/', create_task, name='create_task'),
-    path('tasks/<int:task_id>/', task, name='get_task'),
-    path('tasks/<int:task_id>/update/', task, name='update_task'),
-    path('tasks/<int:task_id>/delete/', task, name='delete_task'),
+    path('tasks/<int:task_id>/', task, name='task'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .serializers import TaskSerializer
 from .models import Task
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+
+@swagger_auto_schema(
+    method='POST',
+    operation_description='Create a new task',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'title': openapi.Schema(type=openapi.TYPE_STRING),
+            'description': openapi.Schema(type=openapi.TYPE_STRING),
+        },
+        required=['title', 'description'],
+    ),
+    responses={
+        201: 'Task created successfully',
+        400: 'Invalid data provided',
+    }
+)
 @api_view(['POST'])
 def create_task(request):
     serializer = TaskSerializer(data=request.data)
@@ -13,6 +33,23 @@ def create_task(request):
     return Response(serializer.errors, status=400)
 
 
+@swagger_auto_schema(
+    method='GET',
+    operation_description='Retrieve all tasks',
+    manual_parameters=[
+        openapi.Parameter(
+            name='status',
+            in_=openapi.IN_QUERY,
+            description='Status filter for tasks',
+            required=False,
+            type=openapi.TYPE_STRING,
+            enum=['all', 'completed', 'incomplete'],
+        ),
+    ],
+    responses={
+        200: 'Tasks retrieved successfully',
+    }
+)
 @api_view(['GET'])
 def get_all_tasks(request):
     status = request.query_params.get('status')
@@ -28,9 +65,42 @@ def get_all_tasks(request):
     return Response(serializer.data)
 
 
+@swagger_auto_schema(
+    method='GET',
+    operation_description='Retrieve a specific task',
+    responses={
+        200: 'Task details retrieved successfully',
+        404: 'Task not found',
+    }
+)
+@swagger_auto_schema(
+    method='PATCH',
+    operation_description='Update a specific task',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'title': openapi.Schema(type=openapi.TYPE_STRING),
+            'description': openapi.Schema(type=openapi.TYPE_STRING),
+            'completed': openapi.Schema(type=openapi.TYPE_BOOLEAN),
+        },
+        required=None,
+    ),
+    responses={
+        200: 'Task updated successfully',
+        400: 'Invalid data provided',
+        404: 'Task not found',
+    }
+)
+@swagger_auto_schema(
+    method='DELETE',
+    operation_description='Delete a specific task',
+    responses={
+        204: 'Task deleted successfully',
+        404: 'Task not found',
+    }
+)
 @api_view(['GET', 'PATCH', 'DELETE'])
 def task(request, task_id):
-
     if request.method == 'GET':
         try:
             task = Task.objects.get(id=task_id)
