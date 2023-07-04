9146a98bf64c9e89a15d99630dd27f3156acf32c
Sergey <physiho@yahoo.com>
2023-07-04 18:43:26 +0400

    Add tasks status filtration

--- tasks/views.py
+++ tasks/views.py
@@ -15,7 +15,15 @@ def create_task(request):
 
 @api_view(['GET'])
 def get_all_tasks(request):
-    tasks = Task.objects.all()
+    status = request.query_params.get('status')
+
+    if status == 'completed':
+        tasks = Task.objects.filter(completed=True)
+    elif status == 'incomplete':
+        tasks = Task.objects.filter(completed=False)
+    else:
+        tasks = Task.objects.all()
+
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
